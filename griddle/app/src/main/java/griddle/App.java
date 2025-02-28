81668b028b139a8ee624aef76f2b1b6571f7a2fe
Dalphin32 <dalphin64@gmail.com>
2025-02-28 11:15:25 -0500

    added create functionality

--- griddle/app/src/main/java/griddle/App.java
+++ griddle/app/src/main/java/griddle/App.java
@@ -5,29 +5,56 @@ package griddle;
 
 import static com.mongodb.client.model.Filters.eq;
 
-import org.bson.Document;
+import java.util.Arrays;
 
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import java.util.Scanner;
+
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
 
 
 
 public class App {
     public static void main(String[] args) {
         // Replace the placeholder with your MongoDB deployment's connection string
-        String uri = "mongodb+srv://dalphin64:gamerdude420@thebestclustertoeverexi.espjq.mongodb.net/?retryWrites=true&w=majority&appName=TheBestClusterToEverExist";
+        String uri = "mongodb+srv://junior_user:test1234@nodeapps.5avaj0n.mongodb.net/?retryWrites=true&w=majority&appName=NodeApps";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter product name:");
+        String prod_name = scanner.nextLine();
+        scanner.nextLine();
+
+        System.out.println("Please enter product price:");
+        String prod_price = scanner.nextLine();
+        scanner.nextLine();
+
+        System.out.println("Please enter product description:");
+        String prod_desc = scanner.nextLine();
+        scanner.nextLine();
 
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-            MongoCollection<Document> collection = database.getCollection("movies");
+            MongoDatabase database = mongoClient.getDatabase("PWD");
+            MongoCollection<Document> collection = database.getCollection("Products");
 
-            Document doc = collection.find(eq("title", "Back to the Future")).first();
-            if (doc != null) {
-                System.out.println(doc.toJson());
-            } else {
-                System.out.println("No matching documents found.");
+            try {
+                // Inserts a sample document describing a movie into the collection
+                InsertOneResult result = collection.insertOne(new Document()
+                        .append("_id", new ObjectId())
+                        .append("title", prod_name)
+                        .append("price", prod_price)
+                        .append("description", prod_desc));
+                // Prints the ID of the inserted document
+                System.out.println("Success!\n" + "Name: "+prod_name+" Price: "+prod_price+" Description: "+prod_desc);
+            
+            // Prints a message if any exceptions occur during the operation
+            } catch (MongoException me) {
+                System.err.println("Unable to insert due to an error: " + me);
             }
         }
     }
